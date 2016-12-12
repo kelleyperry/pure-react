3d048eb71b810d1b4d94c297a39bb9a8386eaa6b
Kelley Perry <kelleyperry@Kelleys-iMac.local>
2016-12-12 00:21:35 -0700

    fetch json

--- credit-card/src/index.js
+++ credit-card/src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import moment from 'moment';
+import "./index.css";
 
 var cardInfo = {
   bankname: "Bank of America",
-  ccnumber: 1234567887654321,
+  ccnumber: 1534561228876513,
   ccv: 1234,
-  validthru: "2016-07-30 21:24:37",
+  validthru: "2018-07-30 21:24:37",
   cardholder: "Kelley Perry"
 };
 
@@ -13,28 +15,39 @@ var CreditCard = React.createClass({
   render: function() {
     return (
       <div className='credit-card'>
-        <div>{cardInfo.bankname}</div>
-        <CCNumber />
-        <div>{cardInfo.ccv}</div>
-        <EXPDate />
-        <div>{cardInfo.cardholder}</div>
+        <div className="bank-name">{cardInfo.bankname}</div>
+        <CCNumber className="card-number" ccnumber={cardInfo.ccnumber} />
+        <div className="ccv-number">{cardInfo.ccv}</div>
+        <EXPDate className="expiration-date" validthru={cardInfo.validthru} />
+        <div className="card-holder">{cardInfo.cardholder}</div>
       </div>
     );
   }
 });
 
 var CCNumber = React.createClass({
+  divideAddSpace: function() {
+    var num = this.props.ccnumber;
+    var str = num.toString();
+    var regex = /(.{4})(?!$)/g;
+    var subst = '$1 ';
+    var result = str.replace(regex, subst);
+    return result;
+  },
   render: function() {
     return(
-      <div></div>
+      <div className={this.props.className}>{this.divideAddSpace()}</div>
     );
   }
 });
 
 var EXPDate = React.createClass({
+  computeMonthYear: function() {
+    return moment(this.props.validthru).format("MM/YY");
+  },
   render: function() {
     return(
-      <div></div>
+      <div className={this.props.className}><span>valid<br />thru</span>{this.computeMonthYear()}</div>
     );
   }
 });
